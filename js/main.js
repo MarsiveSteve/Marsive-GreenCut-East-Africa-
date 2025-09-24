2acc31dccf90b3b6749dafe98bce7db5177df62f
MarsiveSteve <stephkymanih@gmail.com>
2025-09-24 16:34:48 +0300

    dsdsf

--- js/main.js
+++
@@ -1,9 +0,0 @@
-// Simple interactivity
-document.addEventListener("DOMContentLoaded", () => {
-  const bookBtn = document.querySelector(".btn");
-  if (bookBtn) {
-    bookBtn.addEventListener("click", () => {
-      alert("Redirecting to booking services...");
-    });
-  }
-});
