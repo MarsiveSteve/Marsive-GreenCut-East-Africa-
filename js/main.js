337215d9ba8c9eeedf5a6eb1eb7dc00f6500ed3e
Full stack Dev <stephkymanih@gmail.com>
2025-09-28 21:44:26 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,3 +2,11 @@ function toggleMenu() {
   const navLinks = document.querySelector('.nav-links');
   navLinks.classList.toggle('active');
 }
+
+// Toggle mobile nav menu
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
