8b40a92d64f27a414b9dbdac8f71ba53fa317fe0
Full stack Dev <stephkymanih@gmail.com>
2025-10-07 19:59:25 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,13 +6,4 @@ hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
 
-const text = "Comprehensive Lawn Care"; // The text to type out
-let i = 0;
-function typeText() {
-  if (i < text.length) {
-    document.getElementById("typing-effect").textContent += text.charAt(i); // Add one character at a time
-    i++;
-    setTimeout(typeText, 100); // Call this function again after 100ms
-  }
-}
-typeText(); // Start typing the text
+ 
